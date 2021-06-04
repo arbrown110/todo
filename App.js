385d306a3ee3b4b03a0811038a7f1913a6ff4ebe
Angel_Brown <arbrown110@gmail.com>
2021-06-03 21:39:23 -0400

    style todos.

--- App.js
+++ App.js
@@ -33,6 +33,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-
   },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  },
+
+
 });
