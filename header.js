7d1fa76fc1e2521bfdc05eef0b375e4b5bab0a35
Angel_Brown <arbrown110@gmail.com>
2021-06-03 21:58:00 -0400

    adding header to todos.

---
+++ header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function Header() {
+    return (
+        <View>
+           <text> My list of Todos</text> 
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
