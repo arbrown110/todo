f50a5b63fffd8a218566dbda0e1198256cce2c33
Angel_Brown <arbrown110@gmail.com>
2021-06-03 21:15:27 -0400

    insert list and set up flatlist.

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    {text: 'buy food', key: '1' },
+    {text: 'gas car', key: '2' },
+    {text: 'get ready for the week', key: '3' }
+  ]);
+
+
   return (
     <View style={styles.container}>
+      {/* header */}
+      <View style={styles.content}>
 
+       {/* todo form */}
+        <View style={styles.list}>
+          <FlatList 
+          
+          />
+        </View>
+      </View>
     </View>
   );
 }
