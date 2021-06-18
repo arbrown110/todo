317a55341306c4b36333e462eac5e7d07df64e72
Angel_Brown <arbrown110@gmail.com>
2021-06-17 21:50:59 -0400

    adding useState.

--- addTodo.js
+++ addTodo.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, TextInput, Button } from 'react-native';
 
 export default function AddTodo() {
+    const [text, setText] = useState('');
 
     const changeHandler= () => {
-        
+
     }
     return(
         <View>
