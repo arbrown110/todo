e4b6a2fb16d372c5c2757f63b8d8a5f2360f08b5
Angel_Brown <arbrown110@gmail.com>
2021-06-17 21:53:13 -0400

    adding value for textInput.

--- addTodo.js
+++ addTodo.js
@@ -4,14 +4,14 @@ import { StyleSheet, Text, TextInput, Button } from 'react-native';
 export default function AddTodo() {
     const [text, setText] = useState('');
 
-    const changeHandler= () => {
-
+    const changeHandler= (val) => {
+        setText(val);
     }
     return(
         <View>
             <TextInput 
                 placeholder= 'What needs to be done....'
-                onChangeText={changeHandler}
+                onChangeText={(val) => changeHandler(val)}
             />
         </View>
     )
