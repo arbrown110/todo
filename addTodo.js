4eb7a9d81127bb1326df7570931af1a7f00fb310
Angel_Brown <arbrown110@gmail.com>
2021-06-17 21:48:33 -0400

    adding text inPput nd onchange handler.

--- addTodo.js
+++ addTodo.js
@@ -2,5 +2,16 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, TextInput, Button } from 'react-native';
 
 export default function AddTodo() {
-    
+
+    const changeHandler= () => {
+        
+    }
+    return(
+        <View>
+            <TextInput 
+                placeholder= 'What needs to be done....'
+                onChangeText={changeHandler}
+            />
+        </View>
+    )
 }
