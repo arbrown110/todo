efc9b2a1b071d5e91e3321f351ad65f11d95d77b
Angel_Brown <arbrown110@gmail.com>
2021-07-01 22:02:27 -0400

    testing onpress event.

--- App.js
+++ App.js
@@ -38,7 +38,7 @@ const submitHandler = (text) => {
 
   return (
     <TouchableWithoutFeedback onPress={() => {
-      
+      console.log('leave me')
     }}>
       <View style={styles.container}>
         <Header />
