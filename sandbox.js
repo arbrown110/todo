cbedc06910040625bee4b8ddc95308c55f3d5a68
Angel_Brown <arbrown110@gmail.com>
2021-07-08 20:24:55 -0400

    building sandox stlyesheet.

--- sandbox.js
+++ sandbox.js
@@ -6,14 +6,21 @@ import {StyleSheet, Text, View } from 'react-native';
 export default  function Sandbox() {
     return( 
         
-        <Text>
-            sandbox    
-         </Text>
+        <View style={styles.container}>
+             <Text style={styles.boxOne}>one</Text>
+             <Text style={styles.boxTwo}>two</Text>
+             <Text style={styles.boxThree}>three</Text>
+             <Text style={styles.boxFour}>four</Text>
+         </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        paddingTop: 50,
+        backgroundColor: '#ddd',
+    }
 });
 
 
